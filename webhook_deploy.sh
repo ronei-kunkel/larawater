cafac1a6227f3290988a7a085a16006613b2646b
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-10-08 09:17:48 -0300

    [DEPLOY] Debug webhook request

--- webhook_deploy.sh
+++ webhook_deploy.sh
@@ -3,11 +3,6 @@
 while true; do
   request=""
   { 
-    # Read the incoming request line by line and store it in the "request" variable
-    while IFS= read -r line; do
-      request="$request$line"$'\n'
-    done
-
     # Check if the request is not null and then log it
     if [[ -n "$request" ]]; then
       echo -e "Request:\n$request"
