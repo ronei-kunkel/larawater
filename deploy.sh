97db01eb745ef45c6840dd10a3cff8ce490b1faf
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-10-08 07:09:38 -0300

    [DEPLOY] Conditional deploy

--- deploy.sh
+++ deploy.sh
@@ -12,24 +12,31 @@ docker build -t larawater .
 # para o container que está atualmente rodando com a penúltima versão do projeto
 if $(docker ps -q -f name=larawater-app); then
   docker stop larawater-app
+
+  # renomeia o contaner com a antepenúltima versão do projeto para ser removida
+  if $(docker ps -q -f name=larawater-app-last); then
+    docker rename larawater-app-last larawater-app-removed
+  fi
+
+  # renomeia o container com a penúltima versão do projeto
+  if $(docker ps -q -f name=larawater-app); then
+    docker rename larawater-app larawater-app-last
+  fi
 fi
 
-# renomeia o contaner com a antepenúltima versão do projeto para ser removida
-if $(docker ps -q -f name=larawater-app-last); then
-  docker rename larawater-app-last larawater-app-removed
-fi
+if ! $(docker ps -q -f name=larawater-app) ; then
 
-# renomeia o container com a penúltima versão do projeto
-if $(docker ps -q -f name=larawater-app); then
-  docker rename larawater-app larawater-app-last
-fi
+  # roda um novo container com a imagem contendo a última versão do projeto
+  docker run -p 80:80 --name larawater-app larawater 
 
-# roda um novo container com a imagem contendo a última versão do projeto
-if ! docker run -p 80:80 --name larawater-app larawater ; then
+  # remove o container com a antepenúltima versão do projeto
+  if $(docker ps -q -f name=larawater-app-removed); then
+    docker rm larawater-app-removed
+  fi
+
+else
+
+  # inicia o container com a penúltima versão do projeto
   docker start larawater-app-last
 fi
 
-# remove o container com a antepenúltima versão do projeto
-if $(docker ps -q -f name=larawater-app-removed); then
-  docker rm larawater-app-removed
-fi
