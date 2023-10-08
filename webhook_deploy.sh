adf7e07d1bb9e52bfab82f6e6af53a43b8f8114b
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-10-08 09:06:31 -0300

    [DEPLOY] Authorizing webhook request model

--- webhook_deploy.sh
+++ webhook_deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 while true; do
-  # Listen for incoming connections on port 9991
-  { echo -ne "HTTP/1.1 200 OK\r\n\r\n"; } | nc -l -p 9991 -q 1
-
-  # Run the deploy.sh script
-  ./deploy.sh
+  { 
+    read -r request
+    if [[ $request == *"Authorization: Bearer $VALID_KEY"* ]]; then
+      echo -ne "HTTP/1.1 200 OK\r\n\r\n"
+      # Run the deploy.sh script
+      ./deploy.sh
+    else
+      echo -ne "HTTP/1.1 401 Unauthorized\r\n\r\n"
+    fi
+  } | nc -l -p 9991 -q 1
 done
