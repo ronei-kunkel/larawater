76df047c55221263b2f7761cbc56e5fba7c361b4
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-10-08 09:07:35 -0300

    [DEPLOY] Debug webhook request

--- webhook_deploy.sh
+++ webhook_deploy.sh
@@ -2,13 +2,7 @@
 
 while true; do
   { 
-    read -r request
-    if [[ $request == *"Authorization: Bearer $VALID_KEY"* ]]; then
-      echo -ne "HTTP/1.1 200 OK\r\n\r\n"
-      # Run the deploy.sh script
-      ./deploy.sh
-    else
-      echo -ne "HTTP/1.1 401 Unauthorized\r\n\r\n"
-    fi
-  } | nc -l -p 9991 -q 1
+    # Log the entire request
+    echo -e "Request:\n$request"
+  }
 done
