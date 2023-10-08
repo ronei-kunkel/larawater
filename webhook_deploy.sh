c22000820ca2679f660a65b436404cfc7c6ec80f
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-10-08 09:19:14 -0300

    [DEPLOY] Debug webhook request

--- webhook_deploy.sh
+++ webhook_deploy.sh
@@ -3,10 +3,9 @@
 while true; do
   request=""
   { 
-
     # Check if the request is not null and then log it
     if [[ -n "$request" ]]; then
       echo -e "Request:\n$request"
     fi
-  }
+  } | nc -l -p 9991 -q 1
 done
