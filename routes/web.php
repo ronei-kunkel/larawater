75d8dfd9065c2361043cc95fa32f1ed4ee52807d
Ronei Kunkel <ronei.kunkel@gmail.com>
2023-10-08 15:27:13 -0300

    [DEPLOY] Script webhook deploy

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-  echo('<h1>Doc in development</h1><br><p>Try the collection for now: <a href="https://github.com/ronei-kunkel/larawater/blob/main/collection/larawater.postman_collection.json">Link</a></p>');
+  echo('<h1>Doc in development </h1><br><p>Try the collection for now: <a href="https://github.com/ronei-kunkel/larawater/blob/main/collection/larawater.postman_collection.json">Link</a></p>');
 });
 
